652745961b6a836755ffe61a25d71bae60466830
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-07-25 23:41:20 +0800

    refactor: remove vim-better-whitespace
    https://github.com/ntpeters/vim-better-whitespace/issues/158

--- dot_config/nvim/config.lua
+++ dot_config/nvim/config.lua
@@ -89,7 +89,7 @@ linters.setup {
 -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
   -- Neovim plugin for building a sync base16 colorscheme. Includes support for Treesitter and LSP highlight groups.
-  { "RRethy/nvim-base16",             commit = "da2a27c" },
+  { "RRethy/nvim-base16",       commit = "da2a27c" },
   -- A unified, minimal, extensible interface for lightning-fast movements in the visible editor area
   {
     "ggandor/leap.nvim",
@@ -107,21 +107,19 @@ lvim.plugins = {
     end
   },
   -- Standalone UI for nvim-lsp progress
-  { "j-hui/fidget.nvim",              commit = "492492e" },
-  -- Better whitespace highlighting for Vim
-  { "ntpeters/vim-better-whitespace", commit = "c5afbe9" },
+  { "j-hui/fidget.nvim",        commit = "492492e" },
   -- repeat.vim: enable repeating supported plugin maps with "."
-  { "tpope/vim-repeat",               commit = "24afe92" },
+  { "tpope/vim-repeat",         commit = "24afe92" },
   -- surround.vim: Delete/change/add parentheses/quotes/XML-tags/much more with ease
-  { "tpope/vim-surround",             commit = "bf3480d" },
+  { "tpope/vim-surround",       commit = "bf3480d" },
   -- unimpaired.vim: Pairs of handy bracket mappings
-  { "tpope/vim-unimpaired",           commit = "9842718" },
+  { "tpope/vim-unimpaired",     commit = "9842718" },
   -- zoomwintab vim plugin
-  { "troydm/zoomwintab.vim",          commit = "7a354f3" },
+  { "troydm/zoomwintab.vim",    commit = "7a354f3" },
   -- Neovim plugin to preview the contents of the registers
-  { "tversteeg/registers.nvim",       commit = "f354159" },
+  { "tversteeg/registers.nvim", commit = "f354159" },
   -- Make Vim handle line and column numbers in file names with a minimum of fuss
-  { "wsdjeg/vim-fetch",               commit = "0a6ab17" }
+  { "wsdjeg/vim-fetch",         commit = "0a6ab17" }
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
